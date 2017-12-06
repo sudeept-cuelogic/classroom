f590eaec094f0d49e9cecc6e7468c66258640a21
sudeep.cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-06 18:39:54 +0530

    added registration functionality.

--- app/modules/application_module.js
+++ app/modules/application_module.js
@@ -1,27 +1,27 @@
 var application = (function() {
-  let phones = JSON.parse(localStorage.phones || '[]')
-  let emails = JSON.parse(localStorage.emails || '[]')
+  let phone_dir = JSON.parse(localStorage.phone_dir || '[]')
+  let email_dir = JSON.parse(localStorage.email_dir || '[]')
 
-  var _initializeData = (function () {
-    _parseData(students);
-    _parseData(teachers);
-    _saveLocalStorage();
-  })();
-
-  function _parseData (data) {
+  var _parseData = function (data) {
     Object.keys(data).map( (email) => {
       localStorage.setItem(email, JSON.stringify(data[email]));
-      if (!phones.includes(data[email]['phone'])) {
-        phones.push(data[email]['phone']);
+      if (data[email]['phone'] && !phone_dir.includes(data[email]['phone'])) {
+        phone_dir.push(data[email]['phone']);
       }
-      if (!emails.includes(email)) {
-        emails.push(email);
+      if (!email_dir.includes(email)) {
+        email_dir.push(email);
       }
     });
   };
 
-  function _saveLocalStorage() {
-    localStorage.setItem('phones', JSON.stringify(phones));
-    localStorage.setItem('emails', JSON.stringify(emails));
+  var _saveLocalStorage = function () {
+    localStorage.setItem('phone_dir', JSON.stringify(phone_dir));
+    localStorage.setItem('email_dir', JSON.stringify(email_dir));
   }
+
+  var _initializeData = (function () {
+    _parseData(students[0]);
+    _parseData(teachers[0]);
+    _saveLocalStorage();
+  })();
 })();
--- app/modules/session_module.js
+++ app/modules/session_module.js
@@ -1,41 +1,76 @@
 var session = (function () {
-  let phones = JSON.stringify(localStorage.getItem('phones'));
-  let email = J
-  var checkValidSession = function () {
-
-  };
-
-  var login = function () {
-    alert('login clicked');
-  };
-
-  var register = function () {
-    let options = {};
-    let optionKeys = ['firstName', 'lastName', 'email', 'phone', 'password'];
-    optionKeys.map((id) => {
-      options[id] = document.getElementById(id).value
-    });
-    if (_isRegistrationPresent(options.email)) {
-      alert('Email ID already present!!');
-    } else if(_isPhonePresent(options.phone)) {
-      alert('Phone number entered is already present!!');
-    } else {
-      // add data to students and write json to persist data
-      // add new students data to localStorage
-    }
-  };
+  let phone_dir = JSON.parse(localStorage.getItem('phone_dir') || '[]');
+  let email_dir = JSON.parse(localStorage.getItem('email_dir') || '[]');
 
   var _isPhonePresent = function(phone) {
-    alert(phone);
+    return phone_dir.includes(phone);
   };
 
   var _isRegistrationPresent = function (email) {
-    alert(email);
+    return email_dir.includes(email);
+  };
+
+  var _saveLocalStorage = function(options) {
+    localStorage.setItem('phone_dir', JSON.stringify(phone_dir));
+    localStorage.setItem('email_dir', JSON.stringify(email_dir));
+    localStorage.setItem(options['email'], JSON.stringify(options));
+  };
+
+  var _writeToJsonFile = function(students) {
+    debugger
+  };
+
+  var _addPhone = function(phone) {
+    if (phone && !phone_dir.includes(phone)) {
+      phone_dir.push(phone);
+    }
+  };
+
+  var _addEmail = function(email) {
+    if (email && !email_dir.includes(email)) {
+      email_dir.push(email);
+    }
+  };
+
+  var _getFormData = function(keys) {
+    let formData = {};
+    keys.map((id) => {
+      formData[id] = document.getElementById(id).value;
+    });
+    return formData;
+  };
+
+  var _isValidSession = function () {
+    debugger
+  };
+
+  var login = function () {
+    let optionKeys = ['login_email', 'login_password'];
+    let formData = _getFormData(optionKeys);
+    if (_isValidSession()) {
+      sessionStorage.setItem('email', formData['login_email'])
+    }
+  };
+
+  var register = function () {
+    let optionKeys = ['firstName', 'lastName', 'email', 'phone', 'password'];
+    let formData = _getFormData(optionKeys);
+    formData['role'] = 'student';
+
+    if (_isRegistrationPresent(formData.email)) {
+      alert('Email ID already present!!');
+    } else if(_isPhonePresent(formData.phone)) {
+      alert('Phone number entered is already present!!');
+    } else {
+      _addPhone(formData['phone']);
+      _addEmail(formData['email']);
+      students[0][formData[email]] = formData;
+      _writeToJsonFile(students);
+    }
   };
 
   return {
     login: login,
-    register: register,
-    checkSession: checkValidSession
+    register: register
   };
 })();
