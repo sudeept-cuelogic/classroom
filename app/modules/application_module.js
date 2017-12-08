2dfd507ca310dae5edcec83dcbcd5dc26294c42d
sudeep.cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-08 12:49:20 +0530

    saved data to localStorage of window.

--- app/modules/application_module.js
+++ app/modules/application_module.js
@@ -1,11 +1,11 @@
 var application = (function() {
-  let phone_dir = JSON.parse(localStorage.phone_dir || '[]')
-  let email_dir = JSON.parse(localStorage.email_dir || '[]')
+  let phone_dir = JSON.parse(window.localStorage.phone_dir || '[]')
+  let email_dir = JSON.parse(window.localStorage.email_dir || '[]')
 
   var _parseData = function (data) {
     Object.keys(data).map( (email) => {
       if (!window.localStorage.getItem(email)) {
-        localStorage.setItem(email, JSON.stringify(data[email]));
+        window.localStorage.setItem(email, JSON.stringify(data[email]));
       }
       if (data[email]['phone'] && !phone_dir.includes(data[email]['phone'])) {
         phone_dir.push(data[email]['phone']);
@@ -17,8 +17,8 @@ var application = (function() {
   };
 
   var _saveLocalStorage = function () {
-    localStorage.setItem('phone_dir', JSON.stringify(phone_dir));
-    localStorage.setItem('email_dir', JSON.stringify(email_dir));
+    window.localStorage.setItem('phone_dir', JSON.stringify(phone_dir));
+    window.localStorage.setItem('email_dir', JSON.stringify(email_dir));
   }
 
   var _initializeData = (function () {
